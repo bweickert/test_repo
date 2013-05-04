28e2595b9f2018856c53cde17fc89e96562c9bbb
benny <bweickert@oopdev.de>
2013-05-04 17:26:11 +0200

    tests for character.m added

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Benjamin Weickert on 04.05.13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
